a6fa82de84b85906350ca13a428d5567175e7c4f
Peter Gvizd <gvizd.peter@gmail.com>
2021-11-06 14:22:19 +0100

    metric per inspec result

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 docker build -t inspec-vector:latest .
 
-docker run -e "INSPEC_TARGET=/tmp/tests/test-profile" -e "HOSTNAME=test-hostname" -p 9598:9598 -v ${PWD}/tests:/tmp/tests -v ${PWD}/vector.yml:/etc/vector/vector.yml inspec-vector:latest -c /etc/vector/vector.yml
+docker run --env-file ${PWD}/.env -p 9598:9598 -v ${PWD}/tests:/tmp/tests -v ${PWD}/vector.yml:/etc/vector/vector.yml inspec-vector:latest -c /etc/vector/vector.yml
